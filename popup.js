7ecb2eb25c6026ae9efed458fe3170054ae9f811
CeliaLachenal <59375489+CeliaLachenal@users.noreply.github.com>
2024-09-09 10:57:56 +0200

    modif du manifest afin qu'on recupère le fichier json. fetch du json . le jsons'affiche en conssole. il faudra nettoyer le code et supprimmer ce qui ne sert pas

--- popup.js
+++ popup.js
@@ -1,17 +1,53 @@
-console.log("Coucou !");
 
-/* fetch('quotes.json')
+
+// On transforme le chemin relatif de notre fichier json en URL afin de pouvoir effectuer un fetch dessus
+
+
+fetch(chrome.runtime.getURL('quotes.json'))
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-    console.log(data.quote);  // Output: la citation
+    console.log(data);  // Ceci affichera l'objet JSON dans la console
+    return(data);
   })
   .catch(function(error) {
-    console.error('Error loading JSON:', error);
-  });  */
+    console.error("Erreur lors du chargement du fichier JSON : ", error);
+  });
 
- /*  console.log(data.quote); */
+
+function afficher() {
+
+  document.getElementById("citation").popup.innerHTML= "<p></p>";
+
+}
+afficher();
+
+document.getElementById("genere").addEventListener('click', afficher);
+
+
+
+
+
+
+
+//On fait un fetch sur notre fichier json
+const local = "quotes.json";
+
+async function recupererCitation () {
+  const requete = await fetch(local, {
+    method: 'GET'
+  });
+  
+  if(!requete.ok) {
+    alert('Un problème est survenu.');
+  } else {
+    let donnees = await requete.json();
+    // console.log(donnees);
+    document.querySelector('span').textContent = donnees.EUR.last;
+  }
+}
+
 
 function afficher(){
 
