2278687071b534b1951739dd9344ce8be3ae2ed2
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2024-09-10 16:03:30 +0200

    fonctionnalité catégories terminée

--- popup.js
+++ popup.js
@@ -3,6 +3,8 @@
 // On transforme le chemin relatif de notre fichier json en URL afin de pouvoir effectuer un fetch dessus
 
 let allQuotes;
+let sagesse = [];
+let motivation = []
 
 fetch(chrome.runtime.getURL('quotes.json'))
   .then(function(response) {
@@ -14,6 +16,9 @@ fetch(chrome.runtime.getURL('quotes.json'))
 
     allQuotes = data;
     console.log(allQuotes);
+
+
+    
     afficher(data);
    //continuer le math.random+
     console.log("citation albert einstein : ", data.quotes[2].quote, data.quotes[2].author);
@@ -36,10 +41,33 @@ function afficher(dataFetch) {
   console.log("qu'affiche tu?");
 
   let randomId = Math.floor(Math.random() * dataFetch.quotes.length);
-  console.log(randomId);
+  console.log("randomId : ", randomId);
   document.getElementById("citation").innerHTML= dataFetch.quotes[randomId].quote;
   document.getElementById("auteur").innerHTML= dataFetch.quotes[randomId].author;
 
+
+  let sagesseId = [0,1,2,3,4,11,12,13,14,15,16,17,18,20,23,24,26,27,30,31,32,33,34,35,36,37,38,39,42,43];
+  sagesse = sagesseId.map(i => dataFetch.quotes[i]);
+
+  let motivationId = [1,2,4,5,6,7,8,9,10,13,19,20,21,22,25,28,29,32,35,36,37,40,41];
+  motivation = motivationId.map(i => dataFetch.quotes[i]);
+}
+
+
+function afficherSagesse() {
+  let randomSagesseId = Math.floor(Math.random() * sagesse.length);
+  console.log(sagesse);
+  document.getElementById("citation").innerHTML= sagesse[randomSagesseId].quote;
+  document.getElementById("auteur").innerHTML= sagesse[randomSagesseId].author;
+
+
+}
+
+function afficherMotivation() {
+  let randomMotivationId = Math.floor(Math.random() * motivation.length);
+  console.log(motivation);
+  document.getElementById("citation").innerHTML = motivation[randomMotivationId].quote;
+  document.getElementById("auteur").innerHTML = motivation[randomMotivationId].author;
 }
 
 
@@ -47,7 +75,18 @@ document.getElementById("aleatoire").addEventListener('click', function() {
   afficher(allQuotes);
 });
 
+document.getElementById("sagesse").addEventListener('click', function() {
+  afficherSagesse();
+  
+});
 
+document.getElementById("motivation").addEventListener('click', function() {
+   afficherMotivation();
+});
+
+
+
+d
 
 
 
