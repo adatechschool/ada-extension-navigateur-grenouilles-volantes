5f8ffe8157fdea44b391c787c97433ec50f6c971
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2024-09-11 17:27:31 +0200

    retrait des console.log et commentaires, insertion du texte d'erreur

--- popup.js
+++ popup.js
@@ -10,26 +10,18 @@ let motivation = [];
 //fonction de déclenchement du bouton ok (fonctionnalité recherche) 
 document.getElementById("ok").addEventListener('click', function() {
 let mot = champRecherche.value;
-console.log(mot)
 cherche(mot)
 });
 
 
 fetch(chrome.runtime.getURL('quotes.json'))
   .then(function(response) {
-    /* console.log(response); */
     return response.json();
   })
   .then(function(data) {
-    console.log(data);  // Ceci affichera l'objet JSON dans la console
     allQuotes = data;
-    console.log(allQuotes);
-
     afficher(data);
-   //continuer le math.random+
-    console.log("citation albert einstein : ", data.quotes[2].quote, data.quotes[2].author);
     return(data);
-    
   })
   
   .catch(function(error) {
@@ -42,11 +34,9 @@ function afficher(dataFetch) {
     console.error("Paramètre dataFetch de la fonction afficher ne doit pas être undefined ");
     return;
   }
-  console.log("function afficher(dataFetch) : ",dataFetch);
-
 
   let randomId = Math.floor(Math.random() * dataFetch.quotes.length);
-  console.log("randomId : ", randomId);
+
   document.getElementById("citation").innerHTML= dataFetch.quotes[randomId].quote;
   document.getElementById("auteur").innerHTML= dataFetch.quotes[randomId].author;
 
@@ -64,7 +54,7 @@ function afficher(dataFetch) {
 
 function afficherSagesse() {
   let randomSagesseId = Math.floor(Math.random() * sagesse.length);
-  console.log(sagesse);
+
   document.getElementById("citation").innerHTML= sagesse[randomSagesseId].quote;
   document.getElementById("auteur").innerHTML= sagesse[randomSagesseId].author;
 }
@@ -72,7 +62,7 @@ function afficherSagesse() {
 
 function afficherMotivation() {
   let randomMotivationId = Math.floor(Math.random() * motivation.length);
-  console.log(motivation);
+
   document.getElementById("citation").innerHTML = motivation[randomMotivationId].quote;
   document.getElementById("auteur").innerHTML = motivation[randomMotivationId].author;
 }
@@ -97,84 +87,24 @@ function cherche(mot){
   let rechercheParMot = [];
   let rechercheParAuteur = [];
   let tableau = allQuotes.quotes
-  console.log(tableau)
-  // for (const property in allQuotes.quotes) {
-  //   console.log(`${property}: ${allQuotes.quotes[property]}`);
-  // }
-
-
-console.log(mot)
+ 
   for (let i = 0; i < tableau.length; i++ ) {
     let citation = tableau[i].quote
     let auteur = tableau[i].author
-    console.log(auteur)
-    console.log(citation)
     if (citation.includes(mot)) {
-      console.log("le mot est bien inclus dans une citation");
       rechercheParMot.push(citation);
       rechercheParAuteur.push(auteur);
-
     }
   }
-    console.log(rechercheParMot)
-    console.log(rechercheParAuteur)
+
   
-  if (rechercheParMot != null) {
+  if (rechercheParMot.length != 0) {
     let randomIdCitation = Math.floor(Math.random() * rechercheParMot.length); 
-    console.log(rechercheParMot[randomIdCitation].quote)
-    console.log(rechercheParMot[randomIdCitation].author)
-    console.log(rechercheParMot)
-    console.log(rechercheParMot[randomIdCitation])
-    
-    
     document.getElementById("citation").innerHTML = rechercheParMot[randomIdCitation];
-
-
-
     document.getElementById("auteur").innerHTML = rechercheParAuteur[randomIdCitation];
   
   } else {
-    
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//On fait un fetch sur notre fichier json
-/* const local = "quotes.json";
-
-async function recupererCitation () {
-  const requete = await fetch(local, {
-    method: 'GET'
-  });
-  
-  if(!requete.ok) {
-    alert('Un problème est survenu.');
-  } else {
-    let donnees = await requete.json();
-    // console.log(donnees);
-    document.querySelector('span').textContent = donnees.EUR.last;
+    document.getElementById("citation").innerHTML = "Oh non... aucune citation ne contient ce mot! Essaye autre chose!"
+    document.getElementById("auteur").innerHTML = ""
   }
 }
-
-
-function afficher(){
-
-  document.getElementById("citation").innerHTML= "L'enfer c'est les autres ";
-
-}
-
-afficher();
-document.getElementById("genere").addEventListener('click', afficher); */
